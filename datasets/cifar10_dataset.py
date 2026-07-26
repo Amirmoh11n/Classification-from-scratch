8a1b1de15769bc99a4740ed456ddca1cd28ef017
AMIRMOHAMMADNASHALJI <112077542+Amirmoh11n@users.noreply.github.com>
2026-07-26 10:39:55 +0330

    add new files

--- datasets/cifar10_dataset.py
+++ datasets/cifar10_dataset.py
@@ -2,10 +2,16 @@ import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 
+
 class CIFAR10DataModule:
 
-    def __init__(self, batch_size):
+    def __init__(
+        self,
+        batch_size,
+        data_dir="./data"
+    ):
         self.batch_size = batch_size
+        self.data_dir = data_dir
 
     def get_loaders(self):
 
@@ -18,29 +24,31 @@ class CIFAR10DataModule:
         ])
 
         train_dataset = torchvision.datasets.CIFAR10(
-            root="./data",
+            root=self.data_dir,
             train=True,
-            download=True,
+            download=False,
             transform=transform
         )
 
         test_dataset = torchvision.datasets.CIFAR10(
-            root="./data",
+            root=self.data_dir,
             train=False,
-            download=True,
+            download=False,
             transform=transform
         )
 
         train_loader = DataLoader(
             train_dataset,
             batch_size=self.batch_size,
-            shuffle=True
+            shuffle=True,
+            num_workers=2
         )
 
         test_loader = DataLoader(
             test_dataset,
             batch_size=self.batch_size,
-            shuffle=False
+            shuffle=False,
+            num_workers=2
         )
 
         return train_loader, test_loader
